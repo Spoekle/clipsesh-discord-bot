b8c97a9372b6fa13839833fe4e9a6c39dfd8cee4
Spoekle <thiangraber@gmail.com>
2024-06-19 12:41:53 +0200

    fixed authentication expiration

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import os
 import requests
 import discord
+import logging
+import schedule
+import time
 from yt_dlp import YoutubeDL
 from config import DISCORD_BOT_TOKEN, UPLOADBOT_USERNAME, UPLOADBOT_PASSWORD, BACKEND_URL, CLIP_CHANNEL_ID
 
@@ -8,20 +11,33 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = discord.Client(intents=intents)
 
+logging.basicConfig(filename='bot.log', level=logging.DEBUG, 
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
 def get_backend_token():
     response = requests.post(f'{BACKEND_URL}/api/users/login', json={
         'username': UPLOADBOT_USERNAME,
         'password': UPLOADBOT_PASSWORD
     })
     response.raise_for_status()
+    logging.debug(f'Backend token: {response.json()["token"]}')
     return response.json()['token']
+
+def refresh_token():
+    global BACKEND_TOKEN
+    BACKEND_TOKEN = get_backend_token()
+    logging.debug(f'Refreshed backend token: {BACKEND_TOKEN}')
 
 @client.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
-    global BACKEND_TOKEN
-    BACKEND_TOKEN = get_backend_token()
+    print(f'Logged in as {client.user}')
+    refresh_token()
+    schedule.every(50).minutes.do(refresh_token)
     await client.change_presence(activity=discord.CustomActivity(name='Fetching your clips...'))
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 @client.event
 async def on_message(message):
@@ -29,44 +45,52 @@ async def on_message(message):
         return
 
     if 'youtube.com' in message.content or 'youtu.be' in message.content or 'twitch.tv' in message.content:
-        await message.add_reaction('🔄')
+        # await message.add_reaction('🔄')
         url = message.content.strip()
         ydl_opts = {
             'outtmpl': 'downloads/%(id)s.%(ext)s',
             'ratelimit': 20 * 1024 * 1024,
         }
+        logging.debug(f'Received message with URL: {url}')
 
         with YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=True)
-            print(info)
+            logging.debug(f'YoutubeDL info: {info}')
             filename = ydl.prepare_filename(info)
             streamer = info.get('creator', info.get('channel'))
 
     elif message.attachments and 'cdn.discordapp.com' in message.attachments[0].url:
-        await message.add_reaction('🔄')
+        # await message.add_reaction('🔄')
         split_v1 = str(message.attachments).split("filename='")[1]
         filename = str(split_v1).split("' ")[0]
+        logging.debug(f'Filename from message attachments: {filename}')
         if filename.endswith(".mp4"):
             filename = "downloads/{}".format(filename)
+            logging.debug(f'New filename: {filename}')
 
             os.makedirs(os.path.dirname(filename), exist_ok=True)
 
             await message.attachments[0].save(fp=filename)
+            logging.debug(f'Saved attachment to: {filename}')
             streamer = message.author.name
 
-    # Upload to backend
     with open(filename, 'rb') as f:
         files = {'clip': f}
         data = {'streamer': streamer}
         headers = {'Authorization': f'Bearer {BACKEND_TOKEN}'}
+        logging.debug(f'Sending POST request to {BACKEND_URL}/api/clips/upload with streamer: {streamer}')
         response = requests.post(f'{BACKEND_URL}/api/clips/upload', files=files, data=data, headers=headers)
-        await message.clear_reaction('🔄')
+        logging.debug(f'Response from server: {response.text}')
+        # await message.clear_reaction('🔄')
 
     if response.status_code == 200:
-        await message.add_reaction('✅')
+        # await message.add_reaction('✅')
+        logging.debug('Response status code is 200, removing file.')
         os.remove(filename)
     else:
-        await message.add_reaction('❌')
+        # await message.add_reaction('❌')
+        logging.error(f'Response status code is not 200, it is: {response.status_code}. Removing file.')
         os.remove(filename)
 
+logging.info('Running client with DISCORD_BOT_TOKEN.')
 client.run(DISCORD_BOT_TOKEN)
